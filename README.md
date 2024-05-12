380c6a5a7b342c9f1973dcb9521a4a295ee775b0
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-05-13 00:19:18 +0900

    add: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ekimemo-map-database
+このリポジトリは [駅データ](https://github.com/Seo-4d696b75/station_database) にて提供されている駅情報を [ekimemo-map-app](https://github.com/hideki0403/ekimemo-map-app) 用に加工してリリースするためのリポジトリです。
+
+----
+[station_database](https://github.com/Seo-4d696b75/station_database) by Seo-4d696b75, [Licensed under CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
---
+++ build.ts
@@ -0,0 +1,38 @@
+import { join } from 'https://deno.land/std@0.224.0/path/mod.ts'
+import { encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
+
+const rootPath = './station_database/out/main'
+
+try {
+    Deno.statSync(rootPath)
+} catch {
+    console.error('station_database not found')
+    Deno.exit(1)
+}
+
+function readJson<T>(path: string) {
+    const p = join(rootPath, path)
+
+    try {
+        const data = Deno.readTextFileSync(p)
+        return JSON.parse(data) as T
+    } catch {
+        return null
+    }
+}
+
+const line: Record<string, any>[] = []
+
+for (const item of readJson<Record<string, string>[]>('line.json') ?? []) {
+    const lineData = readJson<Record<string, any>>(`line/${item.code}.json`)!
+    const polylineData = readJson<Record<string, any>>(`polyline/${item.code}.json`)
+    lineData['polyline_list'] = polylineData
+    if (!polylineData) console.warn(`warn: polyline ${item.code} not found`)
+    line.push(lineData)
+}
+
+Deno.writeFileSync('station_database.msgpack', encode({
+    station: readJson('station.json'),
+    tree: readJson('tree.json'),
+    line
+}))
