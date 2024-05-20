dae9613784579856ae93909a806aeaf6af97a4e7
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-05-20 18:06:05 +0900

    Revert "change: geojsonを配列のみに置換する処理を追加"
    This reverts commit eef8d8c0d089eef59735c899a65fa5a55b7a0b0a.

--- build.ts
+++ build.ts
@@ -21,57 +21,6 @@ function readJson<T>(path: string) {
     }
 }
 
-function validateGeometryData(data: unknown) {
-    // 与えられたobjectが [[[lat, lon], [lat, lon], ...], [[lat, lon], [lat, lon], ...], ...] の形式であるかどうかを判定する
-    if (!Array.isArray(data)) return false
-    if (!Array.isArray(data[0])) return false
-    if (!Array.isArray(data[0][0])) return false
-    if (typeof data[0][0][0] !== 'number') return false
-    return true
-}
-
-function convertGeojson(data: Record<string, any> | null) {
-    if (data) {
-        switch (data.type) {
-            case 'FeatureCollection': {
-                const result: Record<string, any>[] = []
-                for (const feature of data.features) {
-                    const geometry = feature.geometry
-                    // FeatureCollectionの子(feature)は必ずLineStringが来るものとして扱う
-                    if (geometry.type !== 'LineString') throw new Error('error: FeatureCollectionの子がLineStringではありません')
-                    result.push(geometry.coordinates)
-                }
-
-                if (!validateGeometryData(result)) throw new Error('error: FeatureCollectionのGeometryDataが不正です')
-
-                return {
-                    type: 'polyline',
-                    data: result
-                }
-            }
-
-            case 'Feature': {
-                const geometry = data.geometry
-                if (geometry.type !== 'LineString' && geometry.type !== 'Polygon') throw new Error('error: FeatureのtypeがLineStringまたはPolygonではありません')
-
-                const result = geometry.type === 'LineString' ? [geometry.coordinates] : geometry.coordinates
-
-                if (!validateGeometryData(result)) throw new Error('error: FeatureのGeometryDataが不正です')
-
-                return {
-                    type: geometry.type === 'LineString' ? 'polyline' : 'polygon',
-                    data: result
-                }
-            }
-        }
-    }
-
-    return {
-        type: 'polyline',
-        data: []
-    }
-}
-
 const line: Record<string, any>[] = []
 
 for (const item of readJson<Record<string, string>[]>('line.json') ?? []) {
@@ -80,21 +29,14 @@ for (const item of readJson<Record<string, string>[]>('line.json') ?? []) {
 
     if (!polylineData) console.warn(`warn: polyline ${item.code} not found`)
 
-    lineData['polyline_list'] = convertGeojson(polylineData)
+    lineData['polyline_list'] = polylineData
     lineData['station_list'] = lineData['station_list'].map((x: Record<string, string>) => x.id)
 
     line.push(lineData)
 }
 
-const station: Record<string, any>[] = []
-
-for (const item of readJson<Record<string, any>[]>('station.json') ?? []) {
-    item['voronoi'] = convertGeojson(item['voronoi'])
-    station.push(item)
-}
-
 Deno.writeFileSync('station_database.msgpack', encode({
-    station,
-    line,
+    station: readJson('station.json'),
     tree: readJson('tree.json'),
+    line
 }))
